87d51939495c346ff5859df6fd6a67fd0e01604e
abbayo <abbabgm@gmail.com>
2022-05-23 07:56:44 +0100

    new file:   hardhat-tutorial/constants.js 	new file:   hardhat-tutorial/contracts/CryptoDevsDAO.sol 	new file:   hardhat-tutorial/contracts/FakeNFTMarketplace.sol 	new file:   hardhat-tutorial/scripts/deploy.js 	new file:   my-app/constants.js 	new file:   my-app/pages/index.js 	new file:   my-app/public/cryptodevs/0.svg 	new file:   my-app/styles/Home.module.css

---
+++ hardhat-tutorial/constants.js
@@ -0,0 +1,4 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS = "0x17298649E2810C08DedFeEf77f06cD294A853337";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
